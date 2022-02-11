8ed6ea4096c2a335b456614871d0b4aa52ce6be1
Todd Pangilinan <pang.todd@gmail.com>
2022-02-11 15:19:28 -0800

    update styles.css, scripts.js, and index.html to get user interface working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
-    event.preventDeafult()
+    event.preventDefault();
+    $("#output").show();
+    const name=$("input#userName").val();
+    const userNum= parseInt($("input#userNumber").val());
+    mrRoboto(userNum, name);
+    $(".name").append(name)
+    $(".results").append(updatedArray.join(","))
   });
 
-function mrRoboto(num){
-  numberArray=[];
-  updatedArray=[];
+  function mrRoboto(num, name){
+    numberArray=[];
+    updatedArray=[];
 
-  for (let i = 0; i <= num; i += 1) {
-    numberArray.push(i)
+    for (let i = 0; i <= num; i += 1) {
+      numberArray.push(i)
+    };
+
+    numberArray.forEach(function(j){
+      if(j.toString().includes("3")){
+        updatedArray.push('"Won\'t you be my neighbor,'+name+'?"');
+      }else if(j.toString().includes("2")) {
+        updatedArray.push('"Boop!"');
+      }else if(j.toString().includes("1")) {
+        updatedArray.push('"Beep!"');     
+      }else{
+        updatedArray.push(j);
+      }
+    })
   };
-
-  numberArray.forEach(function(j){
-    if(j.toString().includes("3")){
-      updatedArray.push("Won't you be my neighbor?")
-    }else if(j.toString().includes("2")) {
-      updatedArray.push("Boop!")
-    }else if(j.toString().includes("1")) {
-      updatedArray.push("Beep!")      
-    }else{
-      updatedArray.push(j)
-    }
-  })
-};
+});
