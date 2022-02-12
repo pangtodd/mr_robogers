1cd54ab733c2a4a2964a60579c2fc5d1ee5959dd
Todd Pangilinan <pang.todd@gmail.com>
2022-02-12 06:27:58 -0800

    add features: user input/name will now be included in the output on threes. Also have a button that will display reversed output.

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,37 @@ $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     $("#output").show();
+    $("#input").hide();
     const name=$("input#userName").val();
     const userNum= parseInt($("input#userNumber").val());
     mrRoboto(userNum, name);
-    $(".name").append(name)
-    $(".results").append(updatedArray.join(","))
+    $(".name").append(name);
+    $(".results").append(updatedArray.join(",<br>"));
+  });
+
+  $("form.refresh").submit(function() {
+    location.reload(true);
+  });
+
+  $("form#reverse").submit(function(event) {
+    event.preventDefault();
+    $("#output").show();
+    $("#input").hide();
+    const name=$("input#userName").val();
+    const userNum= parseInt($("input#userNumber").val());
+    mrRoboto(userNum, name);
+    const reversedArray=updatedArray.reverse();
+    console.log(reversedArray);
+    $(".name").append(name);
+    $(".results").append(reversedArray.join(",<br>"));
   });
 
   function mrRoboto(num, name){
     numberArray=[];
     updatedArray=[];
-
     for (let i = 0; i <= num; i += 1) {
       numberArray.push(i)
     };
-
     numberArray.forEach(function(j){
       if(j.toString().includes("3")){
         updatedArray.push('"Won\'t you be my neighbor,'+name+'?"');
